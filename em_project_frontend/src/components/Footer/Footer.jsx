da798133e48350bf6830ba10eabddfca574d5ffa
codervandan <dquintana.dev@gmail.com>
2026-06-20 20:48:49 -0700

    finalized css

--- em_project_frontend/src/components/Footer/Footer.jsx
+++ em_project_frontend/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="footer">
-      <p>&copy; 2026 Estilo Mafioso. All rights reserved.</p>
+      <p className="footer__text">&copy; 2026 Estilo Mafioso. All rights reserved.</p>
     </footer>
   );
 }
