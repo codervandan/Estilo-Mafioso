d80872e56a918e70909ba7b34f692d270b8f5452
codervandan <dquintana.dev@gmail.com>
2026-06-25 19:13:45 -0700

    backend

--- em_project_backend/app.js
+++ em_project_backend/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dns = require("dns");
 const cors = require("cors");
 require("dotenv").config();
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const clothingItemsRouter = require("./routes/clothingItems");
 
